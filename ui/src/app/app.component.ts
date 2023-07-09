08d861d531042855c1ec58c094aa66f327469097
Darien Pardinas Diaz <dpar39@gmail.com>
2023-07-08 23:00:59 -0400

    UI improvements

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
-declare var Split : any;
+import { Component, ViewChild } from '@angular/core';
+import { SplitAreaDirective, SplitComponent } from 'angular-split';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ui';
-
-  constructor() {
-  // Split(["#one", "#two"], {
-  //   sizes: [25, 75],
-  // });
-}
+  @ViewChild('split') split: SplitComponent;
+  @ViewChild('area1') area1: SplitAreaDirective;
+  @ViewChild('area2') area2: SplitAreaDirective;
+  constructor() {}
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularSplitModule } from 'angular-split';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OpcUaConnectionComponent } from './opc-ua-connection/opc-ua-connection.component';
 import { OpcUaAddressSpaceComponent } from './opc-ua-address-space/opc-ua-address-space.component';
+import {
+  faCoffee,
+  faPlusSquare,
+  faMinusSquare,
+  faFileUpload,
+} from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -14,11 +24,17 @@ import { OpcUaAddressSpaceComponent } from './opc-ua-address-space/opc-ua-addres
     OpcUaAddressSpaceComponent,
   ],
   imports: [
+    FontAwesomeModule,
     BrowserModule,
     AngularSplitModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(faCoffee, faMinusSquare, faPlusSquare, faFileUpload);
+  }
+}
